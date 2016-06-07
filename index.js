da48b7e9c4b46ad9ddafbd4ee82d9f0076063748
Fedor Troitsky <fedor.troitsky@gmail.com>
2016-06-07 17:44:57 +0300

    Treat String paramter as a JSON file

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ var extend = require('util')._extend;
 var path = require('path');
 
 function handlebars(data, opts) {
-
+	// If the `data` parameter is a String, then treat it as a JSON file.
+ 	if (typeof data === 'string') {
+ 		data = JSON.parse(fs.readFileSync(data));
+ 	}
+ 	
 	var options = opts || {};
 	    
 	//Go through a partials object
